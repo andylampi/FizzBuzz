4f89f45c369f66040fc7bbbbc2f22f66d8ce6e37
andylampi <andylampi>
2021-10-12 19:11:40 +0200

    Creation step1,step2 and step3, update html and css

--- js/steps.js
+++ js/steps.js
@@ -1,4 +1,4 @@
-function step1() {// build an array of 100 elements with Fizz and Buzz
+function step1() {// build an array of 100 elements and return Fizz or Buzz or FizzBuzz
     let arr = []; 
     for(let n = 0; n < 101; n++){
         if((n%5 == 0) && (n%3 == 0)){ // if n is mutiple of both return "FizzBuzz"
@@ -15,9 +15,10 @@ function step1() {// build an array of 100 elements with Fizz and Buzz
         }
     }
     document.getElementById("number").innerHTML = arr;
+    return arr; 
 }
 
-function step2(){ // it print numbers from 1 to 20
+function step2(){ // it print numbers from 1 to 20 and return Fuzz or Buzz or FuzzBozz
     let arr = [];
     for(let n = 0; n < 21; n++){
         if((n%5 == 0) && (n%3 == 0)){ // if n is mutiple of both return "FizzBuzz"
@@ -34,11 +35,31 @@ function step2(){ // it print numbers from 1 to 20
         }
     }
     document.getElementById("number").innerHTML = arr;
+    return arr;
+}
+function step3(){// build an array of 100 elements and return Foo or Boo or FooBoo
+    let arr = [];
+    for(let n = 0; n < 101; n++){
+        if((n%7 == 0) && (n%11 == 0)){ // if n is mutiple of both return "FizzBuzz"
+            arr[n] = "FooBoo"
+        }
+        else if(n == 7 || n%7 == 0){ // if n is 7 o multiple return Fizz
+        arr[n] = "Foo";
+        }
+        else if(n == 11 || n%11==0){ // if n is 11 o multiple return Buzz
+            arr[n] = "Boo <br>";
+        }
+        else{
+            arr[n] = n; // else add
+        }
+    }
+    document.getElementById("number").innerHTML = arr;
+    return arr;
 }
 
 
+exports.step1 = step1;
+exports.step2 = step2;
+exports.step3 = step3;
 
-exports.moduls = {
-    step1,
-    step2,
-}
+
